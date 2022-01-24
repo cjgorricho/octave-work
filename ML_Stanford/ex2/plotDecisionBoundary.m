e6b056f38d1e41db572c783353039e850ac2862c
Carlos A. Gorricho <cgorricho@heptagongroup.co>
2022-01-23 20:54:40 -0500

    actualizaion trabajo

--- ML_Stanford/ex2/plotDecisionBoundary.m
+++ ML_Stanford/ex2/plotDecisionBoundary.m
@@ -16,7 +16,8 @@ if size(X, 2) <= 3
     % Only need 2 points to define a line, so choose two endpoints
     plot_x = [min(X(:,2))-2,  max(X(:,2))+2];
 
-    % Calculate the decision boundary line
+    % Calculate the decision boundary linef
+
     plot_y = (-1./theta(3)).*(theta(2).*plot_x + theta(1));
 
     % Plot, and adjust axes for better viewing
