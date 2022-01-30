4cb9bdcff75e9e5c8bc7b87be4c9dc9577b08800
Carlos A. Gorricho <cgorricho@heptagongroup.co>
2022-01-30 16:05:56 -0500

    Trabajo CAG

--- ML_Stanford/ex1/ex1_multi.m
+++ ML_Stanford/ex1/ex1_multi.m
@@ -82,29 +82,76 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
-num_iters = 400;
+alpha_div = 3;
+iter_mult = 2;
+alpha = 1;
+num_iters = 50;
+max_loops = 5;
 
-% Init Theta and Run Gradient Descent 
-theta = zeros(3, 1);
-[theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+alpha_hist = zeros(max_loops);
+iters_hist = zeros(max_loops);
+theta = zeros(max_loops, 3);
+J_all = NaN(max_loops, num_iters * iter_mult ^ max_loops);
+
+for i = 1:max_loops
+
+    % Init Theta and Run Gradient Descent 
+
+    [theta(i,:), J_history] = gradientDescentMulti(X, y, theta(i, :)', alpha, num_iters);
+    J_history = [J_history; NaN(size(J_all,2)-size(J_history), 1)];
+    J_all(i, :) = J_history;
+    
+    alpha_hist(i) = alpha;
+    iters_hist(i) = num_iters;
+    
+    if i == max_loops 
+      break
+    end
+    
+    alpha = alpha / alpha_div;
+    num_iters = num_iters * iter_mult;
+
+end
 
 % Plot the convergence graph
 figure;
-plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+
 xlabel('Number of iterations');
 ylabel('Cost J');
+hold on;
+
+colors = ['r', 'g', 'y', 'k'];
+axis ([0 200]);
+legend_text = [];
+
+for i = 1:max_loops-1
+  
+    plot(1:numel(J_all(i, :)), J_all(i, :), colors(i), 'LineWidth', 2);
+    st_text = sprintf('Alpha: %.3f Iter: %d', alpha_hist(i), iters_hist(i));
+    legend_text = [legend_text; st_text];
+
+end
+
+plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+st_text = sprintf('Alpha: %.3f Iter: %d', alpha, num_iters);
+legend_text = [legend_text; st_text];
+legend(legend_text, 'fontsize', 12);
+
+hold off;
 
 % Display gradient descent's result
 fprintf('Theta computed from gradient descent: \n');
-fprintf(' %f \n', theta);
+fprintf(' %f \n', theta(max_loops, :)');
 fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+h_norm = ([1650 3]-mu)./sigma;
+h_norm = [1 h_norm];
+
+price = h_norm * theta(max_loops, :)'; % You should change this
 
 
 % ============================================================
@@ -149,7 +196,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [1 1650 3] * theta; % You should change this
 
 
 % ============================================================
