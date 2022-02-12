ccc20d7ebeabe7ac9ce3194d765600890feed44b
Carlos A. Gorricho <cgorricho@heptagongroup.co>
2022-02-11 23:07:56 -0500

    update ex5

--- ML_Stanford/ex5/ex5.m
+++ ML_Stanford/ex5/ex5.m
@@ -170,7 +170,6 @@ fprintf('\nProgram paused. Press enter to continue.\n');
 pause;
 
 
-
 %% =========== Part 7: Learning Curve for Polynomial Regression =============
 %  Now, you will get to experiment with polynomial regression with multiple
 %  values of lambda. The code below runs polynomial regression with 
@@ -183,27 +182,27 @@ lambda = 0;
 
 % Plot training data and fit
 figure(1);
-plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
+plot(X, y, 'rx', 'MarkerSize', 5, 'LineWidth', 1);
 plotFit(min(X), max(X), mu, sigma, theta, p);
 xlabel('Change in water level (x)');
 ylabel('Water flowing out of the dam (y)');
 title (sprintf('Polynomial Regression Fit (lambda = %f)', lambda));
 
 figure(2);
-[error_train, error_val] = ...
-    learningCurve(X_poly, y, X_poly_val, yval, lambda);
-plot(1:m, error_train, 1:m, error_val);
+[error_train, error_val, error_test] = ...
+    learningCurve_cag(X_poly, y, X_poly_val, yval, X_poly_test, ytest,lambda);
+plot(1:m, error_train, 1:m, error_val, 1:m, error_test);
 
 title(sprintf('Polynomial Regression Learning Curve (lambda = %f)', lambda));
 xlabel('Number of training examples')
 ylabel('Error')
 axis([0 13 0 100])
-legend('Train', 'Cross Validation')
+legend('Train', 'Cross Validation', 'Test', 'fontsize', 12)
 
 fprintf('Polynomial Regression (lambda = %f)\n\n', lambda);
-fprintf('# Training Examples\tTrain Error\tCross Validation Error\n');
+fprintf('# Training Examples\tTrain Error\tCross Val Error\t\tTest Error\n');
 for i = 1:m
-    fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
+    fprintf('  \t%d\t\t%f\t%f\t\t%f\n', i, error_train(i), error_val(i), error_test(i));
 end
 
 fprintf('Program paused. Press enter to continue.\n');
