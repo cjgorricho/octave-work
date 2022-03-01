43b25f249fd03fa1b314bca2277381b17aa5cdfb
Carlos A. Gorricho <cgorricho@heptagongroup.co>
2022-03-01 10:56:22 -0500

    Actualizacion trabajo CAG

--- practica/titanic/costFunctionReg.m
+++ practica/titanic/costFunctionReg.m
@@ -20,10 +20,8 @@ grad = zeros(size(theta));
 
 J = (1/m)*sum(-y.*log(sigmoid(X*theta))-(1-y).*log(1-sigmoid(X*theta))) + (lambda/(2*m))*sum(theta(2:end).^2);
 
-grad(1) = (1/m)*sum((sigmoid(X*theta)-y));
-grad(2:end) = ((1/m)*sum((sigmoid(X*theta)-y).*X(:,2:end)))' + (lambda/m)*theta(2:end);
+grad = ((1/m)*sum((sigmoid(X*theta)-y).*X))' + ((lambda/m)*[0; theta(2:end)]);
 
-grad = grad(:);
 
 % =============================================================
 
--- practica/titanic/ml_lr.m
+++ practica/titanic/ml_lr.m
@@ -28,22 +28,23 @@ clear; close all; clc
 %  Col 4: Sex (male: 1, female: 2)
 %  Col 5: Age; where data was missing I put the average for the class, sex and 
 %         survived/not-survived. This should reduce the drag
-%  Col 6: 
-%  Col 7:
+%  Col 6: SibSp - # of siblings or spouse 
+%  Col 7: Parch - # of parents or children
 %  Col 8: Boarding Port (C: 1, S: 2, Q:3)
 
-% Load data - Passenger Class and Sex
+% Load data - Passenger Class (Col3), Sex (Col4), or both
 data = load('train_val.csv');
-X = data(:, [3, 4]); y = data(:, 2);
+X = data(:, [3, 4, 6, 7]); y = data(:, 2);
 
-% Load data - Passenger Age and add 2 polinomials
+% Load data - Passenger Age and add npols polinomials. If commented (%) not used
 X_age = data(:, 5);
-X_age_poly = polyFeatures(X_age, 2);
+npols = 2;
+X_age_poly = polyFeatures(X_age, npols);
 
 % Integreate data
 m = size(X,1);
-X = [ones(m,1), X, X_age_poly];
-
+X = [ones(m,1), X, X_age_poly]; % Use only if age is a feature
+% X = [ones(m,1), X]; % Use only if age is NOT a feature
 
 % Create random training and validation sets
 rand_ind = randperm(m);
@@ -58,18 +59,30 @@ initial_theta = zeros(size(X, 2), 1);
 %initial_theta = ones(size(X, 2), 1); 
 
 % Set regularization parameter lambda
-lambda = 2.5;
+lambda = 2.75;
 
 % Set Options
 options = optimset('MaxIter', 400, 'GradObj', 'on');
 
 % Optimize
-
 costFunction = @(t)(costFunctionReg(t, X_train, y_train, lambda));
 
 [theta, J, exit_flag, output, grad] = ...
 	fminunc(costFunction, initial_theta, options);
 
+% Calculate predicted values with theta and print accuracy results
 p = predict(theta, X_val);
-
 fprintf('Train Accuracy: %f\n', mean(double(p == y_val)) * 100);
+
+% Plot Learning Curve using larningCurve_rand.m adjusted for this exercise
+%[error_train, error_val] = ...
+%  learningCurve_rand(X_train, y_train, X_val, y_val, lambda);
+
+%close all;
+%X_plot = 1:size(error_train, 1);
+%X_plot = X_plot';
+%y_plot = [error_train, error_val];
+%figure;
+%plot(X_plot, y_plot);
+%legend('Train', 'Cross Validation', 'location', 'northeast');
+%axis([0 100 0 10]);
